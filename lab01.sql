275497130f00be0aa07250152d9bc945954042fd
Tonmai323 <pirat0653027802ton@gmail.com>
2025-07-17 17:15:27 +0700

    Update lab01.sql

--- lab01.sql
+++ lab01.sql
@@ -5,22 +5,68 @@ SELECT firstName, LastName
 from Employees
 
 SELECT *
-FROM Employees City
-= 'London'
+FROM Employees
+WHERE City = 'London'
 
 SELECT EmployeeID, FirstName, Lastname
-FROM EmployeesWHERECity
-= 'London'
+FROM Employees
+WHERE City = 'London'
 
-SELECT City, CountryFROM Customers
+SELECT City, Country
+FROM Customers
 
-SELECT DISTINCT City, CountryFROM Customers
+SELECT DISTINCT City, Country
+FROM Customers
 
 SELECT *
-FROM ProductsWHERE Unitprice
+FROM Products
+WHERE Unitprice > 200
 
 SELECT *
-FROM CustomersWHERE City
-='London'OR City ='Vancouver'
+FROM Customers
+WHERE City ='London'
+    OR City ='Vancouver'
 
+SELECT *
+FROM Customers
+WHERE Country = 'USA'
+    OR City = 'Vancouver'
+
+
+SELECT *
+FROM Products
+WHERE UnitPrice >= 50
+    AND UnitsInStock < 20
+
+SELECT *
+FROM Products
+WHERE UnitPrice BETWEEN 50 AND 100
+
+SELECT *
+FROM Customers
+WHERE Country IN ('Brazil','Argentina','Mexico');
+
+SELECT *
+FROM Employees
+WHERE FirstName LIKE 'N%'
+
+SELECT *
+FROM Employees
+WHERE FirstName LIKE '%Y'
+
+SELECT firstname, lastname form
+Employees
+where FirstName LIKE  '%an%'
+
+
+SELECT CompanyName
+from Customers
+whese
+CompanyName like 'A'
+
+Select top 10
+    ProductName , UnitPrice , UnitsInStorck
+form
+Products
+order by UnitsInStock DESC
 
