31369cec685da6b447eba26fd52b5dfbf5d870ee
Tonmai323 <pirat0653027802ton@gmail.com>
2025-07-17 16:00:00 +0700

    Add files via upload

---
+++ lab01.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM Employees
+
+SELECT firstName, LastName
+from Employees
+
+SELECT *
+FROM Employees City
+= 'London'
+
+SELECT EmployeeID, FirstName, Lastname
+FROM EmployeesWHERECity
+= 'London'
+
+SELECT City, CountryFROM Customers
+
+SELECT DISTINCT City, CountryFROM Customers
+
+SELECT *
+FROM ProductsWHERE Unitprice
+
+SELECT *
+FROM CustomersWHERE City
+='London'OR City ='Vancouver'
+
+
